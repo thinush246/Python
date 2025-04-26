83a99d7061b41656dfeb4f3c7e7cfeb47f2a7df9
thinush246 <dd95488@gmail.com>
2025-04-26 20:51:40 +0530

    variables

---
+++ Variables.py
@@ -0,0 +1,17 @@
+#Creating different variables
+x = 5
+y = "john"
+z = 7.3
+
+print(x)
+print(y)
+print(z)
+
+codingal = "codingal"
+print("codingal")
+
+# User Input
+name = input("Enter your name: ")
+print("hello "+ name, "welcome to codingal")
+
+age = int(input("How old are you? "))
