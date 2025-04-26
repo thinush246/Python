265c14d30822aa0097e9f062616fd848447fc422
thinush246 <dd95488@gmail.com>
2025-04-26 20:35:25 +0530

    welcome

---
+++ Programming.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(5)
+
+#Next line after print
+print("Hello Python \n")
+
+#pass multiple arguments for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("Welcome to",end= "*" )
