a9c1abfa16e75db06b9fa813f38b1b00add52161
thinush246 <dd95488@gmail.com>
2025-05-02 20:35:49 +0530

    lesson3

---
+++ Lesson 03/typecasting.py
@@ -0,0 +1,32 @@
+# Assigning Different Variables
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+# Printing Different Variables and their Data Type
+print("original:")
+print("Name:", name)
+print("Data Type of Name is", type(name))
+
+print("Age:", age)
+print("Data Type of Age is", type(age))
+
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("Weight:", weight)
+print("Data Type of weight is", type(weight))
+
+print()
+print()
+
+#Type casting to convert the datatype of variables
+print("After Type Casting....")
+age = str(age)
+print("age after typecasting: ",age)
+print("Data Type of age is", type(age))
+
+weight = int(weight)
+print("weight after typecasting: ",weight)
+print("Data Type of weight is", type(weight))
