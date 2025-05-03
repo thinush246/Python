edf625418517e6e6cb6a8ae461001290eca341f9
thinush246 <dd95488@gmail.com>
2025-05-03 20:51:06 +0530

    percentage

---
+++ Lesson 04/percentage.py
@@ -0,0 +1,17 @@
+# take marks as input from user
+print("Enter Marks Obtained in 3 Subjects: ")
+
+#math = int(input("maths :"))
+#english = int(input("english :"))
+#science = int(input("science :"))
+math = 92
+english = 94
+science = 90
+
+# Let's calculate the percentage of marks
+sum = math + english + science
+print("sum of math, english, and science")
+
+perc = (sum/300)*100 
+
+print(f"Percentage Mark = {perc}")
