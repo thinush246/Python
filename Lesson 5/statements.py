ec82d184a11ac1637c87e32fc516b2dff9d01d46
thinush246 <dd95488@gmail.com>
2025-05-09 20:24:39 +0530

    statement

---
+++ Lesson 5/statements.py
@@ -0,0 +1,11 @@
+num = 3
+if num > 0:
+    print(num, "is a positive number.")
+
+print()
+
+num = -1
+if num > 0:
+    print(num, "is a positive number.")
+else:
+    print(num, "is a negative number.")
