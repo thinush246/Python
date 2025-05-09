4e0ac951d3f4de8770d7f8b803534046f332ca3c
thinush246 <dd95488@gmail.com>
2025-05-09 20:37:40 +0530

    profit

---
+++ Lesson 05/profit-loss.py
@@ -0,0 +1,13 @@
+#costprice =int(input("enter the cost price: "))
+#sellingprice =int(input("enter the selling price: "))
+costprice = 50
+sellingprice = 80
+
+if sellingprice > costprice:
+    print("profit")
+
+    pt=sellingprice-costprice
+    print(pt)
+
+else:
+    print("No profit")
