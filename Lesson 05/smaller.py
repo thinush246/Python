8fb511a7957f2023fa042bda6f6cc1afd7a055ed
thinush246 <dd95488@gmail.com>
2025-05-09 20:50:18 +0530

    smaller

---
+++ Lesson 05/smaller.py
@@ -0,0 +1,11 @@
+#n=int(input("Enter the number:"))
+n = 12
+
+if n < 15 :
+    print(f"{n} is smaller than 15")
+    print("I'm in the IF block")
+else:
+    print(f"{n} is greater than 15")
+    print("I'm in the ELSE block")
+
+print("I'm not in the IF block and I'm not in the ELSE block")
