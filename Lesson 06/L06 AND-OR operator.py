de36a6f98274ee583d914f5a749756f922770c3d
thinush246 <dd95488@gmail.com>
2025-05-10 20:33:15 +0530

    and or operator

---
+++ Lesson 06/L06 AND-OR operator.py
@@ -0,0 +1,23 @@
+a = 23
+b = 24
+c = 22
+
+if a > b or a < c:
+    print("I am using or operator")
+elif a < b and a > c:
+    print("I am using and operator")
+
+print()
+a=10
+b=-10
+c=0
+
+if a > 0 or b > 0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+if b > 0 or c > 0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
---
+++ Lesson 06/L06 NOT equal.py
@@ -0,0 +1,18 @@
+a=10
+b=12
+c=12
+
+print(a != b)
+print(b != c)
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(f"{a} and {b} are different")
+
+a=4
+b=5
+
+if (a==1) != (b==5) :
+    print("Hello")
