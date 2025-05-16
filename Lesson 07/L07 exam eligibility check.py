bd2bbd88433a9d092067216a7f6960c1cc11126c
thinush246 <dd95488@gmail.com>
2025-05-16 20:24:34 +0530

    check

---
+++ Lesson 07/L07 exam eligibility check.py
@@ -0,0 +1,15 @@
+#Take input for the student that he can attend the exam or not 
+#medical_cause = input("did you have a medical cause Y or N: ")
+medical_cause = "N"
+
+#Take input of the attendance
+#atten = int(input("enter the attendance of the student: "))
+atten = 75
+
+if medical_cause == 'Y': #checking the condition 1
+    print ("You are allowed")
+else:
+    if atten>=75: #checking the condition 2
+        print ("Allowed")
+    else:
+        print ("Not allowed")
