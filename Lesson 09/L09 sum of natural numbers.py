a7865aca67124f97875d46e7a8179c79b73968e4
thinush246 <dd95488@gmail.com>
2025-05-23 20:19:23 +0530

    natural numbers

---
+++ Lesson 09/L09 sum of natural numbers.py
@@ -0,0 +1,13 @@
+#Input the value of terms
+#n = int(input("Enter the value of terms: "))
+n = 8
+print()
+
+sum = 0 #initialize sum value
+i = 1 #initialize i value
+
+while i <= n: #loop will run from 1 to n
+    sum = sum + i
+    i = i + 1
+
+print("Sum = ",sum)
