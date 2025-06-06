65828c694b1ccb2934e1715a9077d9457c334569
thinush246 <dd95488@gmail.com>
2025-06-06 19:36:17 +0530

    square root calculator acp

---
+++ Lesson 04/ACP L04.py
@@ -0,0 +1,11 @@
+import math
+
+# Ask the user to enter a number
+#number = float(input("Enter a number to find its square root: "))
+number =  25
+
+# Calculate the square root
+square_root = math.sqrt(number)
+
+# Print the result
+print("The square root of", number, "is", round(square_root, 2))
