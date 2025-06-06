73458a030a8348d505a06a489666b99b72662f27
thinush246 <dd95488@gmail.com>
2025-06-06 20:26:59 +0530

    weather conditions

---
+++ Lesson 13/L13 weather conditions.py
@@ -0,0 +1,8 @@
+def weather_condition():#function declaration
+    print("Weather is pleasant in",x)
+    print("Weather is same in",y)
+
+x = "autumn"
+y = "winter"
+
+weather_condition() #call the function
