985f6cb83674ebab0811ae83086d209224b9bc1e
thinush246 <dd95488@gmail.com>
2025-06-13 20:24:07 +0530

    break

---
+++ Lesson 15/L15 break.py
@@ -0,0 +1,9 @@
+#word = input("Enter a word: ")
+word = "ThinushA"
+
+for i in word:
+    if (i == 'A'):
+        print ("A is found")
+        break # stop the repitition process and get out of the loop
+    else:
+        print("A not found")
