c0eef2fb7671474cc6fce5fd3f1087267769dd8e
thinush246 <dd95488@gmail.com>
2025-06-27 20:25:57 +0530

    lists

---
+++ Lesson 19/L19 Lists.py
@@ -0,0 +1,26 @@
+print("empty list:")
+empty_list = []
+print(empty_list)
+print()
+
+# A list of numbers
+numbers = [1, 2, 3, 4, 5]
+print("list of numbers:")
+print(numbers)
+print()
+
+# Use * operator
+triples = [5, 1, 7] * 3
+print("triple list")
+print(triples)
+print()
+
+#reverse the given list
+aList = [100, 200, 300, 400, 500]
+print("original list:")
+print(aList)
+print()
+
+aList = aList[::-1]
+print("list in reverse order:")
+print(aList)
