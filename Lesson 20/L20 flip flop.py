81ff2f2d88343cf012176f5b39b4070a5f378f5a
thinush246 <dd95488@gmail.com>
2025-06-28 20:35:00 +0530

    flip flop

---
+++ Lesson 20/L20 flip flop.py
@@ -0,0 +1,23 @@
+def palind(r):
+    e = len(r) - 1
+    s = 0
+
+    while(s <= e):
+        if(r[s] != r[e]):
+            return False
+            
+        s += 1
+        e -= 1
+
+    return True
+
+r = (1, 2, 3, 3, 2, 1)
+print(r)
+print()
+
+result = palind(r)
+
+if(result):
+    print("The Tuple is Flip-Flop")
+else:
+    print("The Tuple is not Flip-Flop")
