ec664ce182579a5c116382eff3c6219f084d6775
thinush246 <dd95488@gmail.com>
2025-07-13 07:30:39 +0530

    set intersection

---
+++ Lesson 22/L22 set intersection.py
@@ -0,0 +1,12 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+
+print("Original set elements:")
+print(setx)
+print(sety)
+print()
+
+print("Intersection of two said sets:")
+setz = setx.intersection(sety)
+
+print(setz)
