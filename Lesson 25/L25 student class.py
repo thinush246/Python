e716122db7af9c70a2eedd39e07278bf9eef3fec
thinush246 <dd95488@gmail.com>
2025-07-18 20:25:54 +0530

    student class

---
+++ Lesson 25/L25 student class.py
@@ -0,0 +1,8 @@
+# create a class
+class student:
+    grade = 10 #variable or feature
+    print("Hi I am student of grade",grade)
+
+# create an object
+thinush = student() #object 1
+pranav = student() #object 2
