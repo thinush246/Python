aac494d40ba7214b89876c223531d3a0382b00ce
thinush246 <dd95488@gmail.com>
2025-08-29 20:20:14 +0530

    tkinter

---
+++ Lesson 37/L37 tkinter window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# Create Window
+window = Tk()
+
+# Set the window Title and Geometry
+window.title("First window")
+window.geometry("400x300") # window size: width = 400, height = 300
+
+window.mainloop()
